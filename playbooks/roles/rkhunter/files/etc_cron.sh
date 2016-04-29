b61f9f1cd7e400df577f4bdccd9fb3df2792ca0d
Andrew Chin <andrewtchin@gmail.com>
2016-04-29 16:17:40 -0500

    Move roles to ansible-junk repo

--- playbooks/roles/rkhunter/files/etc_cron.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# --cronjob suppresses colored output and interactive key presses
-# --update keeps definitions up to date
-# --quiet suppresses all output
-/usr/bin/rkhunter --cronjob --update --quiet
